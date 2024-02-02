87425915a4c629d02ced07acec24d67a144f782d
Sai Chandan Mohanty <saichandan68@gmail.com>
2024-02-02 11:57:49 +0530

    cors allow req from frontend

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({ origin: "http://localhost:3000"}));
 app.use(express.json());
 mongoose
   .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/hacker-news")
