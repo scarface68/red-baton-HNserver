7068575e9b29cde8b79291f20533f59bfa246077
Sai Chandan Mohanty <saichandan68@gmail.com>
2024-02-02 14:50:29 +0530

    Add userId to login response

--- controllers/userController.js
+++ controllers/userController.js
@@ -41,7 +41,7 @@ exports.login = async (req, res) => {
     }
 
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY);
-    res.json({ token });
+    res.json({ token, userId: user._id});
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
