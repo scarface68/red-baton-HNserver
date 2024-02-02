9abcf628876b5202f4d56df6663034ce2777999d
Sai Chandan Mohanty <saichandan68@gmail.com>
2024-02-02 13:33:35 +0530

    Update error messages in login function

--- controllers/userController.js
+++ controllers/userController.js
@@ -32,12 +32,12 @@ exports.login = async (req, res) => {
   try {
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(400).json({ error: "Invalid credentials" });
+      return res.status(400).json({ error: "User does not exist signup first" });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(400).json({ error: "Invalid credentials" });
+      return res.status(400).json({ error: "Invalid Password" });
     }
 
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY);
